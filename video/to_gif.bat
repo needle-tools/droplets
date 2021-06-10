f43723d020eba6bb1e0841fa39a6cdba15722131
Marcel Wiessler <marcel@gaisterhand.de>
2021-06-10 10:34:32 +0200

    fix to gif bat for shell

--- video/to_gif.bat
+++ video/to_gif.bat
@@ -1,13 +1,16 @@
 @echo off
+
+SET ffmpeg="%LocalAppData%/Needle/Shell/Video/ffmpeg.exe"
+echo %ffmpeg%
+
 :next
 if "%~1" == "" goto done
-%~dp0/ffmpeg -i "%~1" -pix_fmt yuv420p -an %~n1-temp.mp4
-%~dp0/ffmpeg -i %~n1-temp.mp4 -vf palettegen=stats_mode=diff %~n1-palette.png
-%~dp0/ffmpeg -i %~n1-temp.mp4 -i %~n1-palette.png -filter_complex "paletteuse" -r 14 "%~n1.gif"
-del %~n1-palette.png
-del %~n1-temp.mp4
+%ffmpeg% -i "%~1" -pix_fmt yuv420p -an "%~n1-temp.mp4"
+%ffmpeg% -i "%~n1"-temp.mp4 -vf palettegen=stats_mode=diff "%~n1-palette.png"
+%ffmpeg% -i "%~n1"-temp.mp4 -i "%~n1-palette.png" -filter_complex "paletteuse" -r 14 "%~n1.gif"
+del "%~n1-palette.png"
+del "%~n1-temp.mp4"
 shift
 goto next
 :done
-pause 10
 exit
