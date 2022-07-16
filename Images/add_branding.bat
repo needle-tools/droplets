59d5b6cad444b505bad555fb8ff84e0d13f96b69
Marcel Wiessler <marcel@gaisterhand.de>
2022-07-16 12:31:07 +0200

    gradient droplet

--- Images/add_branding.bat
+++ Images/add_branding.bat
@@ -1,13 +1,24 @@
-@Echo OFF
+@echo off
 
+:next
+if "%~1" == "" goto done
+SET result="needle-%~n1.png"
+echo result %result%
 @REM add dropshadow to image and save to file
-magick input.png ( +clone -background black -shadow 90x9+0+0 ) +swap -background none -layers merge +repage output.png &
+magick "%~1" ( +clone -background black -shadow 90x9+0+0 ) +swap -background none -layers merge +repage %result% &
 @REM get resulting image with shadow dimensions and save to temporary text file
-magick identify -format "%%[width]x%%[height]" output.png > output.txt &
+magick identify -format "%%[width]x%%[height]" "%result%" > output.txt &
 @REM read back text file to variable
 SET /p size=<output.txt
 @REM remove temporary text file
 DEL /F output.txt
 @REM add background gradient and save again
-magick output.png ( -size %size% -define gradient:angle=45 gradient:#99CC33-#F3E600 ) +swap -background none -layers merge output.png
+magick %result% ( -size %size% -define gradient:angle=45 gradient:#99CC33-#F3E600 ) +swap -background none -layers merge %result%
+shift
+goto next
+:done
+timeout 2
+exit
+
+
 
