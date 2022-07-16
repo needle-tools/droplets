4283e2f748f8e5b00a3463630866ef30ab06db39
Marcel Wiessler <marcel@gaisterhand.de>
2022-07-16 12:09:29 +0200

    wip gradient

--- Images/add_branding.bat
+++ Images/add_branding.bat
@@ -1,1 +1,6 @@
-magick convert input.png ( +clone -background black -shadow 30x8+0+0 ) +swap -background none -layers merge +repage output.png
+@REM magick -size 256x128 -define gradient:angle=45 gradient:black-white linear_gradient_east.png &
+magick input.png ( +clone -background black -shadow 90x5+0+0 ) +swap -background none -layers merge +repage output.png &
+@REM SET DIMENSION=$(identify -format %wx%h output.png) & 
+magick output.png ( -size 100x100 -define gradient:angle=45 gradient:#f80-#08f ) +swap -background none -layers merge output.png
+@REM magick convert input.png ( +clone -background black -shadow 90x8+0+0 ) +swap -background none -layers merge output.png
+
