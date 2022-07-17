2237ad08654b180508724e1bdc636acfcc6cf6d6
Marcel Wiessler <marcel@gaisterhand.de>
2022-07-17 11:44:04 +0200

    automatically copy to clipboard

--- Images/add_branding.bat
+++ Images/add_branding.bat
@@ -3,7 +3,7 @@
 :next
 if "%~1" == "" goto done
 @REM dp1 is directory with drive of current file that is being processed
-SET result="%~dp1/%~n1-needle.png"
+SET result=%~dp1/%~n1-needle.png
 echo result %result%
 SET tempfile="%~dp1\needle-%~n1-temp.txt"
 echo tempfile %tempfile%
@@ -16,11 +16,14 @@ SET /p size=<%tempfile%
 @REM remove temporary text file
 DEL /F %tempfile%
 @REM add background gradient and save again
-magick %result% ( -size %size% -define gradient:angle=45 gradient:#99CC33-#F3E600 ) +swap -background none -layers merge %result%
+magick %result% ( -size %size% -define gradient:angle=45 gradient:#99CC33-#F3E600 ) +swap -background none -layers merge %result% 
+echo %result%
+@REM copy to clipboard
+magick  %result% clipboard:
 shift
 goto next
 :done
-timeout 2
+timeout 3
 exit
 
 
