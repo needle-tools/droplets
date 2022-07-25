024cfe6d27cb0fcdb7e63216c0bea6651533d9c5
Felix Herbst <herbst@prefrontalcortex.de>
2022-07-25 13:54:30 +0200

    unsuccessful attempt at supporting GIF branding, png still works 🤞

--- Images/add_branding.bat
+++ Images/add_branding.bat
@@ -6,10 +6,13 @@ if "%~1" == "" goto done
 SET input="%~1"
 echo input %input%
 @REM dp1 is directory with drive of current file that is being processed
-SET result="%~dp1/%~n1-needle.png"
+SET result="%~dp1%~n1-needle%~x1"
 echo result %result%
-SET tempfile="%~dp1\needle-%~n1-temp.txt"
+SET tempfile="%~dp1needle-%~n1-temp.txt"
+SET ext0=%~x1
+SET extension=%ext0:~1%
 echo tempfile %tempfile%
+echo extension %extension%
 
 
 @REM make corners round
@@ -21,7 +24,7 @@ SET /p size=<%tempfile%
 magick identify -format "roundrectangle 0,0,%%[width],%%[height],8,8" %input% > %tempfile% &
 @REM read back text file to variable
 SET /p clip_command=<%tempfile%
-magick -size %size% xc:none -draw "%clip_command%" png:- | magick %input% -alpha Set - -compose DstIn -composite %result%
+magick -size %size% xc:none -draw "%clip_command%" %extension%:- | magick %input% -alpha Set - -compose DstIn -composite %result%
 
 
 @REM add dropshadow to image and save to file
@@ -38,7 +41,7 @@ magick %result% ( -size %size% -define gradient:angle=45 gradient:#62D399-#D7DB0
 @REM @REM read back text file to variable
 @REM SET /p clip_command=<%tempfile%
 @REM echo size %size%
-@REM magick -size %size% xc:none -draw "%clip_command%" png:- | magick %result% -alpha Set - -compose DstIn -composite %result%
+@REM magick -size %size% xc:none -draw "%clip_command%" %extension%:- | magick %result% -alpha Set - -compose DstIn -composite %result%
 
 @REM add logo
 magick %result% ( logo.png -thumbnail x14 -alpha set -channel A -evaluate multiply 0.4 ) -gravity SouthEast -geometry  +5+3 -composite %result%
